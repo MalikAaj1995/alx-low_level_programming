b0ce323a3c1d1b2d355b421e0deabba53bcf2513
Malika <majerrad@gmail.com>
2023-03-16 17:31:48 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - prints the phrase with proper grammar,
+ * main - prints the phrase with proper grammar
  * Return:0 if exited properly, non zero otherwise
  */
 int main(void)
