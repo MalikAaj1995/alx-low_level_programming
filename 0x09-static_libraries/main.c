3bd03da6e26cdd14530b4ab3bd00c68166afb620
Malika <majerrad@gmail.com>
2023-04-08 21:54:22 -0700

    main.c

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -2,6 +2,6 @@
 
 int main(void)
 {
-    puts("\"Attheendoftheday, mygoalwastobethebesthacker\"\n\t- KevinMitnick");
+    puts("\"Attheendoftheday,mygoalwastobethebesthacker\"\n\t-KevinMitnick");
     return (0);
 }
