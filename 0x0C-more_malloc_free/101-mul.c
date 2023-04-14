332f5efbb21747fb093a2d41c8f5c558293bddd3
Malika <majerrad@gmail.com>
2023-04-13 17:31:23 -0700

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -143,8 +143,8 @@ int main(int argc, char *argv[])
 			_putchar(e[ti]);
 	exit(98);
 	}
-		init(a, ln - 1);
-			for (ti = l2 - 1, i = 0; ti >= 0; ti--, i++)
+	init(a, ln - 1);
+	for (ti = l2 - 1, i = 0; ti >= 0; ti--, i++)
 	{
 	t = mul(argv[2][ti], argv[1], l1 - 1, a, (ln - 2) - i);
 	if (t == NULL)
