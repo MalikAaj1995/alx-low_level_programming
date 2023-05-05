7c22d807fd0996958c4fc7310b2e2ffc991ba4d3
Malika <majerrad@gmail.com>
2023-05-05 15:48:55 -0700

    0x14-bit_manipulation

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * clear_bit - sets the value of a bit to 0 at a given index
- * @*n: the number
+ * @n: pointer to  the number to modify
  * @index: starting from 0 of the bit you want to set
  * Return: 1 if it worked, or -1 if an error occurred
  */
