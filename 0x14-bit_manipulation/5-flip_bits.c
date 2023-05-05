bae0af680b93eef8e8bf0a7de37e2229cc6a51a9
Malika <majerrad@gmail.com>
2023-05-05 16:41:47 -0700

    0x14-bit_manipulation

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * flip_bits -  count the number of bit that are diffirent between to given numbers
+ * @n: a positive number
+ * @m: a positive number
+ *
+ * Return: none
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+
