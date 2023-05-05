ad2db92e0cd537c620901051b56d468ea98a9567
Malika <majerrad@gmail.com>
2023-05-04 18:54:47 -0700

    0x14-bit_manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,30 +9,27 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int convert = 0;
-
+	int i;
 	if (b == NULL)
 	{
 		return (0);
 	}
 
-	for (int i = 0; b[i] != '\0'; i++)
-	{
+	for (i = 0; b[i] != '\0'; i++)
 
-	if (b[i] == '0')
 	{
-		convert = convert << 1;
+		if (b[i] == '0')
+
+			convert <<= 1;
+
+		else if (b[i] == '1')
+
+			convert = (convert << 1) | 1;
+
+		else
+
+			return (0);
 	}
 
-	else if (b[i] == '1')
-	{
-		convert = (convert << 1) | 1;
-	}
-
-	else
-	{
-		return (0);
-	}
-
-	}
 	return (convert);
 }
