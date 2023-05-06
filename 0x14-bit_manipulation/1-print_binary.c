1d38de441992e0144b43d1638180087867b4bdcb
Malika <majerrad@gmail.com>
2023-05-05 17:50:44 -0700

    0x14-bit_manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,12 +9,21 @@
 void print_binary(unsigned long int n)
 {
 	int size = sizeof(n) * 8;
+
 	int i;
+
 	int bit;
 
 	for (i = size - 1; i >= 0; i--)
 	{
-		bit = (n & (1 << i)) != 0;
+		if ((n >> i) & 1)
+		{
+			break;
+		}
+	}
+	for (; i >= 0; i--)
+	{
+		bit = (n >> i) & 1;
 		putchar(bit ? '1' : '0');
 	}
 	putchar('\n');
