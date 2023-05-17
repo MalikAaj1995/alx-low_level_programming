679cc344de7d60fcd442119d1e56f10376a3d8d4
Malika <majerrad@gmail.com>
2023-05-16 19:35:00 -0700

    0x15-file_io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,12 +3,12 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define BUF_SIZE 1024
 /**
  *err_exit - return the value of a specific error
  *@exit_code: exit
  *@format: format of the error message
  *@arg: the specific argument caused the error
+ *
  */
 void err_exit(int exit_code, char *format, char *arg)
 {
@@ -24,18 +24,18 @@ void err_exit(int exit_code, char *format, char *arg)
 int main(int argc, char *argv[])
 {
 	char buf[1024];
-	ssize_t bytes_read = 1024, bytes_written;
+	int bytes_read = 1024, bytes_written;
 	int  fd_from, fd_to;
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from < 0)
 		err_exit(98, "Error: Can't read from file %s\n", argv[1]);
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to < 0)
 		err_exit(99, "Error: Can't write to file %s\n", argv[2]);
 	while (bytes_read == 1024)
