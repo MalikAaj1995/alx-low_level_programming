1191bd7e69eadb267dee96ea63891952ad8d568a
Malika ajerrad <majerrad@gmail.com>
2023-07-10 16:59:37 -0700

    0x15-file_io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * read_textfile  - reads a text file and prints it to the POSIX standard output.
- * @filename: name of the file to create 
+ * read_textfile - reads a text file and prints it to the POSIX.
+ * @filename: name of the file to create
  * @letters: the number of letters it should read and print
  * Return: the actual number of letters it could read and print
  */
@@ -11,27 +11,32 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	FILE *fp;
 	size_t bytesRead;
-	char * buffer;
+	char *buffer;
+
 	if (filename == NULL)
 	{
 		return (0);
 	}
+
 	fp = fopen(filename, "r");
 	if (fp == NULL)
-        {
+	{
 		return (0);
-        }
+	}
+
 	buffer = (char *)malloc((letters + 1) * sizeof(char));
 	if (buffer == NULL)
 	{
 		fclose(fp);
 		return (0);
 	}
+
 	bytesRead = fread(buffer, sizeof(char), letters, fp);
 	buffer[bytesRead] = '\0';
 	if (bytesRead > 0)
 	{
 	ssize_t bytesWritten = fwrite(buffer, sizeof(char), bytesRead, stdout);
+
 	if ((size_t) bytesWritten != bytesRead)
 	{
 		free(buffer);
