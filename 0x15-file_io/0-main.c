d043c8d73c03b6603f9ced4cbbcc7b9fc62fcf8e
Malika ajerrad <majerrad@gmail.com>
2023-07-10 17:03:10 -0700

    0x15-file_io

--- 0x15-file_io/0-main.c
+++ 0x15-file_io/0-main.c
@@ -4,7 +4,8 @@
 
 /**
  * main - check the code
- *
+ * @ac: the number of command line arguments passed to the program
+ * @av: contains the command line arguments
  * Return: Always 0.
  */
 int main(int ac, char **av)
