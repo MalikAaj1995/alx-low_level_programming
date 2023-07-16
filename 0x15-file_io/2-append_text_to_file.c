087dedd5bd4ecdecf8ba6d51a39bf8b8beb53641
Malika <majerrad@gmail.com>
2023-07-16 12:08:07 -0700

    0x15-file_io

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,7 +7,7 @@
  * append_text_to_file - at the end of a file
  * @filename:the name of the file
  * @text_content: the NULL terminated string
- * @Return: 1 on success and -1 on failure
+ * Return: 1 on success and -1 on failure
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
